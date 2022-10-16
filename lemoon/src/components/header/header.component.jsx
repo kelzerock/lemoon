81d0b24c0ea165ba0ee0b617bc0e57577e4c9d5d
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-10-16 08:44:53 +0300

    fix: some changes in header

--- lemoon/src/components/header/header.component.jsx
+++ lemoon/src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import Logo from "../logo/logo.components";
 import "./header.styles.scss";
-import lemonImg from '../../assets/img/lemon.png'
+import lemonImg from "../../assets/img/lemon.png";
 
 const Header = () => {
   return (
@@ -8,9 +8,12 @@ const Header = () => {
       <Logo info="small" />
       <div className="header-info">
         <h3>Лови сочную волну</h3>
-        <h5>Lemoon – новая платформа для предпринимателей, где мы собрали все рынки страны в одном месте. Теперь продавать стало ещё проще! </h5>
-        </div>
+        <h5>
+          Lemoon – новая платформа для предпринимателей, где мы собрали все
+          рынки страны в одном месте. Теперь продавать стало ещё проще!{" "}
+        </h5>
         <img className="image-lemon" src={lemonImg} alt="lemon" />
+      </div>
     </header>
   );
 };
